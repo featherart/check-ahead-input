d8bb0394666da5948ab3380568d562e649dfd97e
Feather Knee <fknee@nvidia.com>
2019-08-17 20:12:05 -0700

    first commit

---
+++ src/CheckAhead/CheckAhead.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react'
+import './check-ahead.css'
+
+export const CheckAhead = ({ label }) => {
+  const xSpan = <span>&#10005;</span>
+  const checkSpan = <span>&#10004;</span>
+
+	const [ isDisabled, setEnabled ] = useState(false)
+  const [ isLongEnough, setIsLongEnough ] = useState(false)
+  const [ hasLowerCase, setHasLowerCase ] = useState(false)
+  const [ hasUpperCase, setHasUpperCase ] = useState(false)
+  const [ hasNumbers, setHasNumbers ] = useState(false)
+
+  // Mention useState returns a pair of values
+  // the current state and the function that updates that value
+	const testInput = (input) => {
+    const lowercaseTest = /[a-z]/ // just lowercase letters
+    setHasLowerCase(lowercaseTest.test(input))
+
+    const uppercaseTest = /[A-Z]/ // just uppercase
+    setHasUpperCase(uppercaseTest.test(input))
+
+    const numbersTest = /[0-9]/ // just numbers
+    setHasNumbers(numbersTest.test(input))
+
+    const lengthTest = input.length >= 7
+    setIsLongEnough(lengthTest)
+  }
+
+  // Avoid Gotchas!
+  useEffect(() => {
+    const shouldEnable = (isLongEnough && hasLowerCase && hasUpperCase && hasNumbers)
+    console.log(shouldEnable)
+    setEnabled(shouldEnable)
+  }
+  , [isLongEnough, hasLowerCase, hasNumbers, hasUpperCase])
+
+	return (
+		<div className='check-ahead-container'>
+      <div className='form-container'>
+        <label>
+          {label}
+          <input placeholder='password' type='text' onChange={(e) => testInput(e.target.value)} />
+        </label>
+        <button className='submit-button' type='submit' disabled={!isDisabled}>
+          submit
+        </button>
+      </div>
+      <div className='messages-container'>
+        <div>{isLongEnough ? checkSpan : xSpan} password has 7 characters</div>
+        <div>{hasLowerCase ? checkSpan : xSpan} password has a lowercase letter</div>
+        <div>{hasUpperCase ? checkSpan : xSpan} password has a capital letter</div>
+        <div>{hasNumbers ? checkSpan : xSpan} password has a number</div>
+      </div>
+		</div>
+	)
+}
